7b563d54350384a967e0f3300e6059bec05fa9f2
Mota <pjdrm@ist.utl.pt>
2015-07-16 14:00:31 +0100

    Added param to define a cut off threshold cut off for the keywords of a metro station.
    Filteres keywords in a metro station that are a subset of some other
    keyword.

--- mm/mapgen/cluster_generator_rake.py
+++ mm/mapgen/cluster_generator_rake.py
@@ -18,6 +18,7 @@ class ClusterGeneratorRAKE(metro_station_generator.MetroStationGenerator):
         self.minchars = int(config['minchars'])
         self.maxwords = int(config['maxwords'])
         self.minfreq = int(config['minfreq'])
+        self.minscore = float(config['minscore'])
 
         self.input_JSON = config['input_json']
         self.output_JSON = config['output_json']
@@ -43,6 +44,28 @@ class ClusterGeneratorRAKE(metro_station_generator.MetroStationGenerator):
         clust_dic['cluster_tokens'] = clust_tokens
         return [clust_dic]
     
+    '''
+    Merges elements that are a subset of some other cluster element
+    '''
+    def mergeCluster(self, cluster):
+        wordSplitCluster = []
+        for el in cluster:
+            wordSplitCluster.append(set(el.split(' ')))
+        sorted(wordSplitCluster, key=len)
+        mergedCluster = []
+        for el in wordSplitCluster:
+            flag = False
+            for i in range(len(mergedCluster)):
+                if el.issubset(mergedCluster[i]):
+                    flag = True
+                    break
+            if not flag:
+                mergedCluster.append(el)
+                
+        return [' '.join(x) for x in mergedCluster]
+                    
+        
+        
     def run(self):
         logging.debug('RAKE based Metro Station Generator: run begin')
         self.timeslice_clusters = {}
@@ -51,8 +74,13 @@ class ClusterGeneratorRAKE(metro_station_generator.MetroStationGenerator):
             mergedDocs = self.mergeDocs(current_timeslice)
             rake_object = rake.Rake(stopwords.swList(), self.minchars, self.maxwords, self.minfreq)
             cluster = rake_object.run(mergedDocs)
-            clust = [tup[0] for tup in cluster]
-            self.timeslice_clusters[i] = self.genDicFromClust(clust)
+            clusterFiltered = []
+            for el in cluster:
+                if el[1] <= self.minscore:
+                    break
+                clusterFiltered.append(el[0])
+            merged_cluster = self.mergeCluster(clusterFiltered)
+            self.timeslice_clusters[i] = self.genDicFromClust(merged_cluster)
             
 def construct(config):
     return ClusterGeneratorRAKE(config)  
